4c0cef119ea3f09006ce59a88106142072ac964d
sergiorez-cmd <120671477+sergiorez-cmd@users.noreply.github.com>
2025-11-02 16:02:03 -0300

    Update python-dns-resolver.py

--- python-dns-resolver.py
+++ python-dns-resolver.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 # Programação de redes com python3
 # https://docs.python.org/3/library/socket.html
-# Resolver endereço IP e FQDN de um host na Internet
+# Resolver endereço IP de um host na Internet
 
 import socket
 
-if __name__ == '__main__':
-    hostname = input('Digite um endereço de host www.example.com:')
+add_host = input('Digite um endereço de host www.example.com:')
+
+def resolv(hostname):
     address = socket.gethostbyname(hostname)
     fqdn = socket.getfqdn(hostname)
     print ('O endereço IP de {} é {} FQDN: {}'.format(hostname, address, fqdn))
+
+if __name__ == '__main__':
+    resolv(add_host)
